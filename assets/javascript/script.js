425f7f9141087c48b6592a640d1b555a77a629d5
Rhubarb <edlun066@d.umn.edu>
2019-03-25 11:39:35 -0500

    Cleaned up

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,17 +4,25 @@ $("#add_button").on("click", function(event) {
     event.preventDefault();
 
 
-//2. Grab text from train-name in the form.
-var trainName = $("#name").val().trim();
+//2. Grab text from namein the form.
+var Name = $("#name-input").val().trim();
 
 // 2.5 Append it to table as a new row.
+
+
 //3. Grab text from destination in form and append it in new column, same row.
+var Dst = $("#dst-input").val().trim();
+
 
 //4. Grab text from (military time) train-time in form.
+var Time = $("#time-input").val().trim();
+
 
 //5. Grab text from frequency and append it.
+var Frq = $("#frq-input").val().trim();
 
 //6. Calculate when next train will arrive relative to current time and append it.
+
 
 //7. Calculate the minutes away and append it.
 
