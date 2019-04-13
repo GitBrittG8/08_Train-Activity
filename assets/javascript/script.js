924559c8885a444d022fd3b73f4e51e027085169
Rhubarb <edlun066@d.umn.edu>
2019-04-13 17:42:43 -0500

    Javascript Reset

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,124 +8,12 @@
 
 // 1. Initialize Firebase
 var config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    storageBucket: ""
+    apiKey: "AIzaSyDOhQcF1FSCTHK1-xiFVTh9meYqrm3q6Qo",
+    authDomain: "train-station-62cf4.firebaseapp.com",
+    databaseURL: "https://train-station-62cf4.firebaseio.com",
+    storageBucket: "train-station-62cf4.appspot.com"
   };
   
   firebase.initializeApp(config);
   
   var database = firebase.database();
-  
-  // 2. Button for adding Employees
-$("#add_button").on("click", function(event) {
-    event.preventDefault();
-
-    var Name = $("#name-input").val().trim();
-    var Dst = $("#dst-input").val().trim();
-    var Time = $("#time-input").val().trim();
-    var Frq = $("#frq-input").val().trim();
-    //var Next = ...
-    //var Min = ...
-
-    console.log(Name);
-    console.log(Dst);
-    console.log(Time);
-    console.log(Frq);
-
-    // Creates local "temporary" object for holding employee data
-  var newTrain = {
-    name: Name,
-    destination: Dst,
-    time: Time,
-    frequency: Frq
-  };
-
-  // Uploads employee data to the database
-  database.ref().push(newTrain);
-
-  // Logs everything to console
-  console.log(newTrain.name);
-  console.log(newTrain.destination);
-  console.log(newTrain.time);
-  console.log(newTrain.frequency);
-
-  console.log("Train successfully added");
-
-  // Clears all of the text-boxes
-  $("#-name-input").val("");
-  $("#dst-input").val("");
-  $("#time-input").val("");
-  $("#frq-input").val("");
-});
-
-// 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
-database.ref().on("child_added", function(childSnapshot) {
-  console.log(childSnapshot.val());
-
-  // Store everything into a variable.
-  var trainName = childSnapshot.val().name;
-  var trainDst = childSnapshot.val().destination;
-  var trainTime = childSnapshot.val().time;
-  var trainFrq = childSnapshot.val().frequency;
-
-  console.log(trainName);
-  console.log(trainDst);
-  console.log(trainTime);
-  console.log(trainFrq);
-
-  // Prettify the Next Arrival Time
-  var nextArrivalPretty = moment.unix(nextArrival).format("HH:MM --military time");
-
-
-
-
-
-  // Calculate the next arrival
-
-  var nextArrival = Frq + trainTime;
-  
-  // Calculate the Minutes away
-
-  var minsAway = nextArrival - currentTime;
-
-  console.log(minsAway);
-
-
-
-
-
-
-  // Create the new row
-  var newRow = $("<tr>").append(
-    $("<td>").text(trainName),
-    $("<td>").text(trainDst),
-    $("<td>").text(trainFrq),
-    $("<td>").text(nextArrivalPretty),
-    $("<td>").text(minsAway)
-  );
-
-  // Append the new row to the table
-  $("#train-table > tbody").append(newRow);
-});
-/*
-
-    // Clear localStorage
-    localStorage.clear();
-
-    // Store all content into localStorage
-    localStorage.setItem("name", Name);
-    localStorage.setItem("destination", Dst);
-    localStorage.setItem("time", Time);
-    localStorage.setItem("frequency", Frq);
-});
-
-// By default display the content from localStorage
-$("#name-display").text(localStorage.getItem("name"));
-$("#destination-display").text(localStorage.getItem("destination"));
-$("#time-display").text(localStorage.getItem("time"));
-$("#frequency-display").text(localStorage.getItem("frequency"));
-
-//1. Calculate when next train will arrive relative to current time and append it.
-//2. Calculate the minutes away and append it.
